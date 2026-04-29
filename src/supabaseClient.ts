c256190646be957c2dd2765ced6fe0b00bd3eef5
jardeladp-rgb <jardel.beites@fiesc.com.br>
2026-04-29 14:00:23 -0300

    correção de tudo

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Substitua pelos dados que você pegou no painel do Supabase
-const supabaseUrl = 'SUA_URL_AQUI';
-const supabaseKey = 'SUA_CHAVE_ANON_AQUI';
+const supabaseUrl = 'https://jbapenfzejagwvkbnbdf.supabase.co/rest/v1/'; 
+const supabaseKey = 'sb_publishable_lJxItVLxxcBK5_7uVae5mw_aiBendbe';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
