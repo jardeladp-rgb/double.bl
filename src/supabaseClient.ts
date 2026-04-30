4abb3f3a32ddbddc1b3af4271db200afbeab22dd
jardeladp-rgb <jardel.beites@fiesc.com.br>
2026-04-29 21:20:36 -0300

    correção

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jbapenfzejagwvkbnbdf.supabase.co/rest/v1/'; 
+const supabaseUrl = 'https://jbapenfzejagwvkbnbdf.supabase.co'; 
 const supabaseKey = 'sb_publishable_lJxItVLxxcBK5_7uVae5mw_aiBendbe';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
